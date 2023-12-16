d051359e1f0af74e2a437c41c51acc32a827b648
Evgeny Efimenko <evgeny.efimenko@proton.me>
2023-12-16 14:53:36 +0500

    fix: htmlFors

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,17 +1,19 @@
 import { Switch } from '@/components/ui/switch'
 import { Label } from '@/components/ui/label'
 import SettingsGroup from './SettingsGroup'
+import FontSelect from './controls/FontSelect'
 
 export default function Sidebar() {
   return (
     <aside className='w-80 flex flex-col fixed bg-card top-0 bottom-0 border'>
       <SettingsGroup title='Editor'>
         <fieldset className='flex items-center justify-between'>
-          <Label htmlFor='show-title-bar' className='text-muted-foreground'>
+          <Label htmlFor='show-background' className='text-muted-foreground'>
             Background
           </Label>
-          <Switch id='show-title-bar' />
+          <Switch id='show-background' />
         </fieldset>
+        <FontSelect />
         <fieldset className='flex items-center justify-between'>
           <Label htmlFor='show-title-bar' className='text-muted-foreground'>
             Title Bar
@@ -19,10 +21,10 @@ export default function Sidebar() {
           <Switch id='show-title-bar' />
         </fieldset>
         <fieldset className='flex items-center justify-between'>
-          <Label htmlFor='show-title-bar' className='text-muted-foreground'>
+          <Label htmlFor='show-trafic-lights' className='text-muted-foreground'>
             Trafic Lights
           </Label>
-          <Switch id='show-title-bar' />
+          <Switch id='show-trafic-lights' />
         </fieldset>
       </SettingsGroup>
     </aside>
